6fd07fea680bc6bf68e1facca8cd0053f48821f0
Chi Hung <minhchau.nguyen14@gmail.com>
2018-10-13 15:16:17 +0700

    add map

--- js/main.js
+++ js/main.js
@@ -1,22 +1,6 @@
-
-$('.ui.dropdown').dropdown();
-
-$('.ui.checkbox').checkbox();
-
-$('.circle-weekday')
-  .popup({
-    on: 'hover'
-  })
-  ;
-
 $(document).ready(function () {
   $(".height-100").height($(window).height());
   $(window).resize(function () {
     $(".height-100").height($(window).height());
   });
-});
-
-$('.container-scroll').mousewheel(function (e, delta) {
-  $(this).scrollLeft(this.scrollLeft + (-delta * 40));
-  e.preventDefault();
 });
