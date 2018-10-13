6301a57c96097eb74bb4be5c70e8db09f3519fe8
Chi Hung <minhchau.nguyen14@gmail.com>
2018-10-13 13:47:03 +0700

    replace sematic

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,17 @@
 "use strict";
 
-const del          = require('del');
-const gulp         = require('gulp');
-const sass         = require('gulp-sass');
-const postcss      = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const sourcemaps   = require('gulp-sourcemaps');
-const cleanCss     = require('gulp-clean-css');
-const notify       = require('gulp-notify');
-const htmllint     = require('gulp-htmllint');
-const gutil        = require('gulp-util');
-const bootlint     = require('gulp-bootlint');
-const runSequence  = require('run-sequence');
-const gulpif       = require('gulp-if');
-const minimist     = require('minimist');
-const ejs          = require('gulp-ejs');
-
-const fs           = require('fs');
-const browserslistrc = JSON.parse(fs.readFileSync('./config/.browserslistrc'));
-const cleanCssConfig = JSON.parse(fs.readFileSync('./config/cleanCss.config.json'));
+const del = require('del');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cleanCss = require('gulp-clean-css');
+const notify = require('gulp-notify');
+const gutil = require('gulp-util');
+const runSequence = require('run-sequence');
+const gulpif = require('gulp-if');
+const minimist = require('minimist');
+const ejs = require('gulp-ejs');
 
 /**********************************************
  *
@@ -27,7 +20,7 @@ const cleanCssConfig = JSON.parse(fs.readFileSync('./config/cleanCss.config.json
  **********************************************/
 const options = minimist(process.argv.slice(2), {
   string: 'env',
-  default: {env: process.env.NODE_ENV || 'development'} // NODE_ENVに指定がなければ開発モードをデフォルトにする
+  default: { env: process.env.NODE_ENV || 'development' } // NODE_ENVに指定がなければ開発モードをデフォルトにする
 });
 const isProduction = (options.env === 'production');// $ gulp --env production
 
@@ -38,15 +31,15 @@ console.log('env is ' + options.env);
  *
  *
  **********************************************/
-const baseDir = './';
-const _sourceDir    = baseDir + '_source/';
-const _htmlDir      = baseDir + 'xxxxx/';
-const _scssDir      = [
-  baseDir + 'scss/**/*.scss',
+const _sourceDir = './_source/';
+const _htmlDir = './';
+const _scssDir = [
+  './scss/**/*.scss',
+  './components/sematic-ui/css/*.scss',
   '!./scss/xxx.scss'
 ];
-const _cssDir       = baseDir + 'css/';
-const componentsDir = baseDir + 'components/';
+const _cssDir = './css/';
+const componentsDir = './components/';
 
 const htmlFiles = [
   _htmlDir + '**/*.html',
@@ -68,12 +61,41 @@ gulp.task("scss", function () {
   gulp.src(_scssDir)
     .pipe(sourcemaps.init())
     .pipe(sass())
-    .pipe(postcss([autoprefixer({
-      browsers: browserslistrc
-    })]))
-    .pipe(cleanCss(cleanCssConfig))
+    .pipe(autoprefixer({
+      browsers: [
+        'last 2 versions',
+        'Android 3',
+        'ie <= 9'
+      ]
+    })
+    )
+    .pipe(cleanCss({
+      format: {
+        breaks: { // controls where to insert breaks
+          afterAtRule: true, // controls if a line break comes after an at-rule; e.g. `@charset`; defaults to `false`
+          afterBlockBegins: true, // controls if a line break comes after a block begins; e.g. `@media`; defaults to `false`
+          afterBlockEnds: true, // controls if a line break comes after a block ends, defaults to `false`
+          afterComment: true, // controls if a line break comes after a comment; defaults to `false`
+          afterProperty: true, // controls if a line break comes after a property; defaults to `false`
+          afterRuleBegins: true, // controls if a line break comes after a rule begins; defaults to `false`
+          afterRuleEnds: true, // controls if a line break comes after a rule ends; defaults to `false`
+          beforeBlockEnds: true, // controls if a line break comes before a block ends; defaults to `false`
+          betweenSelectors: true // controls if a line break comes between selectors; defaults to `false`
+        },
+        indentBy: 2, // controls number of characters to indent with; defaults to `0`
+        indentWith: 'space', // controls a character to indent with, can be `'space'` or `'tab'`; defaults to `'space'`
+        spaces: { // controls where to insert spaces
+          aroundSelectorRelation: true, // controls if spaces come around selector relations; e.g. `div > a`; defaults to `false`
+          beforeBlockBegins: true, // controls if a space comes before a block begins; e.g. `.block {`; defaults to `false`
+          beforeValue: true // controls if a space comes before a value; e.g. `width: 1rem`; defaults to `false`
+        },
+        wrapAt: false // controls maximum line length; defaults to `false`
+      },
+      level: 2
+    })
+    )
     .pipe(gulpif(isProduction, cleanCss()))
-    .pipe(sourcemaps.write('.'))
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest(_cssDir))
     .pipe(notify({
       onLast: true,
@@ -99,16 +121,14 @@ gulp.task('copy-source', function () {
     _sourceDir + '**/*.min.css',
     _sourceDir + '**/*.min.js',
     _sourceDir + '**/*-min.js',
-    _sourceDir + '**/bower_components/slick-carousel/slick/slick.css',
-    _sourceDir + '**/bower_components/slick-carousel/slick/ajax-loader.gif',
     '!' + _sourceDir + '**/docs/**',
     '!' + _sourceDir + 'bower_components/matchHeight/jquery.matchHeight-min.js',
     '!' + _sourceDir + 'bower_components/jquery/external/**/*',
     '!' + _sourceDir + 'bower_components/html5shiv/dist/html5shiv-printshiv.min.js',
     '!' + _sourceDir + 'bower_components/respond/dest/respond.matchmedia.addListener.min.js'
   ], {
-    base: _sourceDir
-  })
+      base: _sourceDir
+    })
     .pipe(gulp.dest(componentsDir))
     .pipe(notify({
       onLast: true,
@@ -132,19 +152,6 @@ gulp.task('source', function (callback) {
  *
  *
  **********************************************/
-gulp.task('remove-html', function (cb) {
-  return del([
-    _htmlDir + '*.html'
-  ], {
-    force: false
-  }, cb);
-});
-
-/**********************************************
- *
- *
- *
- **********************************************/
 gulp.task("ejs", function () {
   return gulp.src([
     "ejs/**/*.ejs",
@@ -161,29 +168,7 @@ gulp.task("ejs", function () {
  *
  *
  **********************************************/
-gulp.task('htmllint', function () {
-  return gulp.src(htmlFiles)
-    .pipe(htmllint({
-      'config': 'config/.htmllintrc'
-    }, htmllintReporter));
-});
 
-function htmllintReporter(filepath, issues) {
-  if (issues.length > 0) {
-    issues.forEach(function (issue) {
-      let name = gutil.colors.red('[gulp-htmllint Error] ');
-      let path = gutil.colors.white(filepath + ' [Line: ' + issue.line + ', ' + issue.column + ']: ');
-      let message = issue.msg;
-      let issueCode = gutil.colors.red('(' + issue.code + ') ');
-      let errorMessage = name + path + issueCode + message;
-
-      gutil.log(errorMessage);
-    });
-
-    // process.exitCode = 0;
-    process.exit(0);
-  }
-}
 
 /**********************************************
  *
@@ -194,7 +179,6 @@ gulp.task('html', function (callback) {
   updateSourceTime = Date.now();
 
   runSequence(
-    'remove-html',
     'ejs',
     'htmllint',
     callback
@@ -206,10 +190,7 @@ gulp.task('html', function (callback) {
  *
  *
  **********************************************/
-gulp.task('bootlint', function() {
-  return gulp.src('./index.html')
-    .pipe(bootlint());
-});
+
 
 /**********************************************
  *
@@ -219,25 +200,17 @@ gulp.task('bootlint', function() {
 gulp.task('default', [
   'scss',
   'source',
-  'htmllint',
-  'bootlint',
   // 'html'
 ], function () {
   gulp.watch(_scssDir, [
     'scss'
   ]);
 
-  gulp.watch(htmlFiles, [
-    'htmllint',
-    'bootlint'
+  gulp.watch(_sourceDir + '**/*', [
+    'source'
   ]);
 
-  // gulp.watch(_sourceDir + '**/*', [
-  //   'source'
-  // ]);
-
   // gulp.watch('ejs/**/*.ejs', [
-  //   'remove-html',
   //   'ejs'
   // ]);
 });
